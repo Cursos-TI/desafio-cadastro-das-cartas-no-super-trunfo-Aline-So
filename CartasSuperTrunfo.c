6318af44d6ef9c4aea472c6d5e2923923cfeadc6
Aline-So <maiaraaline581@hotmail.com>
2025-02-18 15:15:27 +0000

    Cadastro das infomações das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,33 +4,33 @@
 
 int main() {
     
-    char nome;
-    int populacao;
-    float area;
-    float PIB;
-    int pontosturisticos;
+    char EstadoA, EstadoB;
+    char CodigodaCartaA01, CodigodaCarta02;
+    char NomedaCidadeA, NomedaCidadeB;
+    int PopulacaoA, PopulacoaB;
+    float AreaA, AreaB;
+    float PIBA, PIBB;
+    int NumerodePontosTuristicosA, NumerodePontosTuristicosB;
+     
+    printf("Bem vindo ao cadastro das cartas do Jogo Super Trunfo!\n");
     
+    printf("Carta: 1\n");
+    printf("Estado A: PE\n");
+    printf("Código: A01 \n");
+    printf("Nome da Cidade: Garanhuns\n");
+    printf("População: 142.506\n");
+    printf("Área: 472,5 km²\n");
+    printf("PIB:2.342.966,00\n");
+    printf("Número de Pontos Turísticos: 7\n");
 
-    printf("Digite o nome da cidade: \n");
-    scanf("%c", &nome);
-
-    printf("Digite o numero da populacao: \n");
-    scanf("%d", &populacao);
-
-    printf("digite o tamanho da area: \n");
-    scanf("%f", &area);
-
-    printf("digite o PIB da cidade: \n");
-    scanf("%f", &PIB);
-
-    printf("Digite o numero de pontos turisticos da cidade");
-    scanf("%d", &pontosturisticos);
-    
-    printf("Digite o nome da cidade: %c");
-    printf("Digite o numero da populacao: %d");
-    printf("digite o tamanho da area: %f");
-    printf("digite o PIB da cidade: %f");
-    printf("Digite o numero de pontos turiscos da cidade: %d");
+    printf("Carta: 2\n");
+    printf("Estado B: PE\n");
+    printf("Código: B02 \n");
+    printf("Nome da Cidade: São Paulo\n");
+    printf("População: 8.664.306 \n");
+    printf("Área: 1.521,20km² \n");
+    printf("PIB:2.719.751.231,00 \n");
+    printf("Número de Pontos Turísticos: 36\n");
 
 
 
