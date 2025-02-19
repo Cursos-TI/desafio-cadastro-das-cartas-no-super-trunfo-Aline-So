163d3d4e6a44d69a5a7c3a46120a847bc37a2562
Aline-So <maiaraaline581@hotmail.com>
2025-02-19 04:52:27 +0000

    Super Trunfo Novato.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,9 +5,9 @@
 int main() {
     
     char EstadoA, EstadoB;
-    char CodigodaCartaA01, CodigodaCarta02;
+    char CodigodaCartaA01, CodigodaCartaB02;
     char NomedaCidadeA, NomedaCidadeB;
-    int PopulacaoA, PopulacoaB;
+    int PopulacaoA, PopulacaoB;
     float AreaA, AreaB;
     float PIBA, PIBB;
     int NumerodePontosTuristicosA, NumerodePontosTuristicosB;
@@ -15,14 +15,29 @@ int main() {
     printf("Bem vindo ao cadastro das cartas do Jogo Super Trunfo!\n");
     
     printf("Carta: 1\n");
-    printf("Estado A: PE\n");
-    printf("Código: A01 \n");
-    printf("Nome da Cidade: Garanhuns\n");
-    printf("População: 142.506\n");
-    printf("Área: 472,5 km²\n");
-    printf("PIB:2.342.966,00\n");
-    printf("Número de Pontos Turísticos: 7\n");
-
+    
+    printf("Estado A: ");
+    scanf("%s", &EstadoB);
+    
+    printf("Código: ");
+    scanf("%s", &CodigodaCartaA01);
+    
+    printf("Nome da Cidade: ");
+    scanf("%s", &NomedaCidadeA);
+    
+    printf("População: ");
+    scanf("%d", &PopulacaoA);
+    
+    printf("Área km²: ");
+    scanf("%f", &AreaA);
+    
+    printf("PIB:");
+    scanf("%f", &PIBA);
+   
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &NumerodePontosTuristicosA);
+   
+   
     printf("Carta: 2\n");
     printf("Estado B: PE\n");
     printf("Código: B02 \n");
